f2182397339d82f95e397d42e0f2488f61974289
Pobitrodeb <pobitrodn@gmail.com>
2022-12-05 21:10:18 +0600

    Phone model and migration file create

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -10,8 +10,32 @@
 
 </head>
 <body>
+    <section class="py-5">
+        <div class="container">
+            <div class="card border-0">
+                <div class="card-header text-center"> Eloquent Relationship  </div>
+                <div class="card-body">
+                    <table class="table table-bordered text-center">
+                        <thead>
+                        <tr>
+                            <td> Sl </td>
+                            <td> Name </td>
+                            <td> Phone</td>
+                        </tr>
+                        </thead>
+                        <tboady>
+                            <tr>
+                                <td> 01</td>
+                                <td> Pobitrodeb </td>
+                                <td> 245152454 </td>
+                            </tr>
+                        </tboady>
+                    </table>
+                </div>
+            </div>
 
-
+        </div>
+    </section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
