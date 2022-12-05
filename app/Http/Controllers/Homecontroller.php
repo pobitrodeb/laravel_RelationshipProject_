302e27962254357eee078ec442dbd8ef03ff4070
Pobitrodeb <pobitrodn@gmail.com>
2022-12-05 20:45:18 +0600

    laravel install

---
+++ app/Http/Controllers/Homecontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Homecontroller extends Controller
+{
+    public function index()
+    {
+        echo 'contratulations';
+    }
+}
